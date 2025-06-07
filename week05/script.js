d28bb42ee8900437190c8c41c36891ddecedbd99
Onkabetse2002 <onkabetsenkabi3@gmail.com>
2025-06-07 13:19:00 +0200

    Submit

---
+++ week05/script.js
@@ -0,0 +1,16 @@
+// Product array
+const products = [
+    { id: 1, name: "Product A" },
+    { id: 2, name: "Product B" },
+    { id: 3, name: "Product C" },
+    { id: 4, name: "Product D" },
+];
+
+// Populate the product select options
+const productSelect = document.getElementById('productName');
+products.forEach(product => {
+    const option = document.createElement('option');
+    option.value = product.id;
+    option.textContent = product.name;
+    productSelect.appendChild(option);
+});
